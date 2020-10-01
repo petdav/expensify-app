cba7d1b54b6e91cd7d32d174a8eccc33423b3683
petdav <petteridavidsson@gmail.com>
2020-10-01 12:39:23 +0300

    Add Expensify styles

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -13,7 +13,6 @@ export const PublicRoute = ({
             <Redirect to="/dashboard" />
         ) : (
             <div>
-                <Header />
                 <Component {...props} />
             </div>
         )
